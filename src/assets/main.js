a5846b188ebaa590d9d38259e015755bf208bd82
Pacha <rodrigonzalez12@gmail.com>
2022-12-14 18:17:26 +1000

    sarasa

--- src/assets/main.js
+++ src/assets/main.js
@@ -23,7 +23,7 @@ async function fetchData(urlApi) {
         <div class="group relative">
             <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
             </div>
             <div class="mt-4 flex justify-between">
             <h3 class="text-sm text-gray-700">
